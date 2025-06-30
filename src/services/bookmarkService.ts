5162298834e8ac9dcd10809e5cd62f354dd464d6
Marcos Prado <marcospradofelix@gmail.com>
2025-06-30 20:59:19 -0300

    feat: adiciona função para obter favoritos de voos

--- src/services/bookmarkService.ts
+++ src/services/bookmarkService.ts
@@ -3,3 +3,7 @@ import api from "@/lib/api";
 export function addBookmark(flightNumber: string) {
     return api.post("/bookmarks", { flightNumber });
 }
+
+export function getBookmarks() {
+  return api.get("/bookmarks").then((res) => res.data);
+}
