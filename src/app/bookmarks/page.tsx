dd647a5d0c3dbf37e3043aeb3757169745cc136a
Marcos Prado <marcospradofelix@gmail.com>
2025-07-19 11:32:01 -0300

    feat: add cached bookmarks flight

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -6,8 +6,12 @@ import { getBookmarks, removeBookmark } from "@/services/bookmarkService";
 import { getColumnsBookmark } from "@/components/ui/columns-bookmarks";
 import Header from "@/components/ui/header";
 import { Flight } from "@/types/flight";
+import { findFlightByFlightNumber } from "@/services/flightService";
+import { RemovedFlight } from "@/types/removed-flights";
 
 export default function BookmarkPage() {
+    const [cachedFlight, setCachedFlight] = useState<RemovedFlight[] | null>([]);
+
     const [flights, setFlights] = useState<Flight[]>([]);
 
     useEffect(() => {
@@ -26,13 +30,36 @@ export default function BookmarkPage() {
     async function handleRemove(flightNumber: string) {
         try {
             await removeBookmark(flightNumber);
-            setFlights((prev) => prev.filter((f) => f.flightNumber !== flightNumber));
+            const flight = await findFlight(flightNumber);
+            await addCachedFlight(flight);
         } catch (error) {
             console.error("Erro ao remover favorito", error);
         }
     }
 
-    const columns = getColumnsBookmark({ onRemove: handleRemove });
+    async function addCachedFlight(flight: Flight) {
+        const removedFlight: RemovedFlight = { ...flight, isRemoved: true };
+        setCachedFlight((prev) => [...(prev || []), removedFlight]);
+    }
+
+    async function removedCachedFlight(flightNumber: string) {
+        setCachedFlight((prev) => prev?.filter((f) => f.flightNumber !== flightNumber) || []);
+    }
+
+    async function findFlight(flightNumber: string) {
+        try {
+            const flight = await findFlightByFlightNumber(flightNumber);
+            return flight;
+        } catch (error) {
+            console.error("Erro: ", error);
+        }
+    }
+
+    async function onFavorited(flightNumber: string) {
+        removedCachedFlight(flightNumber);
+    }
+
+    const columns = getColumnsBookmark({ onRemove: handleRemove, onFavorited: onFavorited});
 
     return (
         <div className="bg-gray-100 min-h-screen">
@@ -47,7 +74,13 @@ export default function BookmarkPage() {
                     {flights.length === 0 ? (
                         <p className="text-center text-gray-500">Nenhum favorito adicionado.</p>
                     ) : (
-                        <DataTable columns={columns} data={flights} />
+                        <DataTable
+                            columns={columns}
+                            data={flights.map((flight) => ({
+                                ...flight,
+                                isRemoved: cachedFlight?.some((f) => f.flightNumber === flight.flightNumber),
+                            }))}
+                        />
                     )}
                 </div>
             </div>
